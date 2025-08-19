a2b213e97bb7af197d8c22ed138fe9a9f7bcee0d
raul <rauulssanchezz@gmail.com>
2025-08-19 15:47:08 +0200

    Cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers.currency_router import router
 
 app = FastAPI(
     title='Currency Change'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
